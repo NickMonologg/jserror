88d5b5a9abbc4b772ecb0c3bdc81d8aced0f143a
Peter Müller <munter@fumle.dk>
2015-10-20 23:34:42 +0200

    Add a bare minimum test

--- test/jserror-test.js
+++ test/jserror-test.js
@@ -7,4 +7,8 @@ describe('jserror', function () {
   it('should return a function', function () {
     return expect(jserror, 'to be a function');
   });
+
+  it('should return an error message in a source comment', function () {
+    return expect(jserror('foo'), 'to contain', '* foo');
+  });
 });
